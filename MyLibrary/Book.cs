c8ba7c5df5b46a768bdc8b5433cd93a42e4b193c
Fabián Camilo <Fabin Camilo@LAPTOP-FF3UB4CB>
2024-12-10 15:36:09 -0500

    Agregar archivos de proyecto.

---
+++ MyLibrary/Book.cs
@@ -0,0 +1,17 @@
+﻿namespace MyLibrary;
+public class Book : Product
+{
+    public string Title { get; set; }
+    public Book(int id, string name, decimal unitPrice, int unitStock, string title) :
+        base(id, name, unitPrice, unitStock)
+    {
+        Title = title;
+    }
+}
+
+public class Book2(int id, string name, decimal unitPrice, int unitStock, string title) :
+        Product(id, name, unitPrice, unitStock)
+{
+    public string Title => title;
+}
+
---
+++ MyLibrary/Product.cs
@@ -0,0 +1,29 @@
+﻿namespace MyLibrary;
+public class Product (int id, string name, decimal unitPrice, int unitInStock)
+{
+    //Field, Campo, Variable
+
+    private int Id_Field;
+
+    //Prpiedades
+    public int Id { get; } = id;
+    public string Name { get; } = name;
+    public decimal UnitPrice { get; } = unitPrice;
+    public int UnitInStock { get; } = unitInStock;
+
+    public bool Discontinued { get; private set; }
+
+    public void SetDiscontinued() => Discontinued = true; 
+    
+    //Herencia
+
+    //public class Producto2(int id, string name, decimal unitPrice, int unitInStock, bool discontinue) 
+    //    : Product (id, name, unitPrice,unitInStock)
+    //{
+    //    public bool Discontinue { get;  set; } = discontinue;
+    //}
+
+
+}
+
+
