b62c76a7fc2125c54898db5e34e471606d80b9f7
Fabian Camilo Marriaga Sierra <fabiancamilo15@msn.com>
2024-12-11 12:05:51 -0500

    Hasta Polimorfismo Sesion 3

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,7 +1,25 @@
-﻿using MyLibrary;
+﻿//using MyLibrary;
 
-int i = 5;
-string Name = "Hello";
+//int i = 5;
+//string Name = "Hello";
 
-Product Leche;
-Leche = new Product(1, "Leche entera", 19, 100);
+//Product Leche;
+//Leche = new Product(1, "Leche entera", 19, 100);
+
+using MyLibrary;
+
+Secretary Maria = new Secretary();
+Maria.Name = "Maria";
+Manager Pedro = new Manager();
+Pedro.Name = "Pedro";
+Developer Miguel = new Developer();
+Miguel.Name = "Miguel";
+
+WriteEmployeeInfo(Maria);
+WriteEmployeeInfo(Pedro);
+WriteEmployeeInfo(Miguel);
+
+void WriteEmployeeInfo(Employee employee)
+    {
+    Console.WriteLine($"{employee.Name}:{ employee.CalculateMonthlyPayment()}");
+}
